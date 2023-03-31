ecb97064ab2f48ae1ae982ac21dde5193c402e61
Santiago Cruz <cruzantiago@gmail.com>
2023-03-31 16:31:01 -0500

    Crear las clases model, mapeo de las clases a tablas con JPA, integracion de las vistas con el diseño preestructurado del curso

--- src/main/java/com/curso/ecommerce/springecommerce/model/Usuario.java
+++ src/main/java/com/curso/ecommerce/springecommerce/model/Usuario.java
@@ -1,6 +1,20 @@
 package com.curso.ecommerce.springecommerce.model;
 
+import java.util.List;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "usuarios")
+
 public class Usuario {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
     private String nombre;
     private String username;
@@ -9,6 +23,13 @@ public class Usuario {
     private String telefono;
     private String tipo;
     private String password;
+
+    @OneToMany(mappedBy = "usuario")
+    private List<Producto> productos;
+
+    @OneToMany(mappedBy = "usuario")
+    private List<Orden> ordenes;
+
 
     public Usuario(Integer id, String nombre, String username, String email, String direccion, String telefono,
             String tipo, String password) {
@@ -87,6 +108,14 @@ public class Usuario {
 
     public void setPassword(String password) {
         this.password = password;
+    }
+
+    public List<Producto> getProductos() {
+        return productos;
+    }
+
+    public void setProductos(List<Producto> productos) {
+        this.productos = productos;
     }
 
     @Override
